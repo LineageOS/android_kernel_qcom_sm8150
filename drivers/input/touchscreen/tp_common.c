f6cbbba330113bb1dbfd1ed7bab1a5e18e5aa225
dianlujitao <dianlujitao@lineageos.org>
2020-02-19 16:48:25 +0800

    drivers: input: touchscreen: Add an interface to expose TP features to userspace
    * Needs to implement and register ops from TP driver
     * Access them via /sys/touchpanel/*
    
    Change-Id: I2baa61c6055ca6f6207ac0d6f627fe03048ccfb6

---
+++ drivers/input/touchscreen/tp_common.c
@@ -0,0 +1,26 @@
+#include <linux/input/tp_common.h>
+
+struct kobject *touchpanel_kobj;
+
+#define TS_ENABLE_FOPS(type)                                                   \
+	int tp_common_set_##type##_ops(struct tp_common_ops *ops)              \
+	{                                                                      \
+		static struct kobj_attribute kattr =                           \
+			__ATTR(type, (S_IWUSR | S_IRUGO), NULL, NULL);         \
+		kattr.show = ops->show;                                        \
+		kattr.store = ops->store;                                      \
+		return sysfs_create_file(touchpanel_kobj, &kattr.attr);        \
+	}
+
+TS_ENABLE_FOPS(double_tap)
+TS_ENABLE_FOPS(fod_status)
+
+static int __init tp_common_init(void)
+{
+	touchpanel_kobj = kobject_create_and_add("touchpanel", NULL);
+	if (!touchpanel_kobj)
+		return -ENOMEM;
+
+	return 0;
+}
+core_initcall(tp_common_init);
