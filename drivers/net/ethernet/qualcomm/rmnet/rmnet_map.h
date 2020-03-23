37457f754072933849ad7eaf97306e9038e44ec1
Subash Abhinov Kasiviswanathan <subashab@codeaurora.org>
2020-03-22 21:02:58 -0600

    BACKPORT: core: Remove dl marker v1 usage
    This removes the unused dl marker v1 definitions, handlers and users
    since these are no longer used.
    
    This also fixes the following CFI warning-
    CFI failure (target: rmnet_perf_core_handle_map_control_end.cfi_jt+0x0/0x4 [rmnet_perf]):
    WARNING: CPU: 1 PID: 0 at kernel/cfi.c:29 __ubsan_handle_cfi_check_fail+0x4c/0x54
    pstate: 60400005 (nZCv daif +PAN -UAO)
    pc: __ubsan_handle_cfi_check_fail+0x4c/0x54
    lr: __ubsan_handle_cfi_check_fail+0x4c/0x54
    Call trace:
    __ubsan_handle_cfi_check_fail+0x4c/0x54
    __cfi_check+0x204/0x220 [rmnet_perf]
    rmnet_map_dl_trl_notify_v2+0x58/0x88 [rmnet_core]
    rmnet_frag_flow_command+0x110/0x120 [rmnet_core]
    rmnet_frag_ingress_handler+0xe0/0x3bc [rmnet_core]
    rmnet_rx_handler+0x1cc/0x2a4 [rmnet_core]
    __netif_receive_skb_core+0x554/0xdc4
    process_backlog$4cc8cf18b485f47de9fc54109f04daea+0x1a4/0x314
    net_rx_action$4cc8cf18b485f47de9fc54109f04daea+0x144/0x578
    __do_softirq+0x250/0x580
    irq_exit+0xcc/0xd0
    handle_IPI+0x228/0x3b0
    efi_header_end+0x148/0x17c
    el1_irq+0x108/0x200
    lpm_cpuidle_enter$a9941074ca35bb1f25355cf2ff310eae+0x57c/0x5c8
    cpuidle_enter_state+0x130/0x334
    cpuidle_enter+0x38/0x50
    do_idle.llvm.4847091502713502628+0x1e4/0x2ec
    cpu_startup_entry+0x24/0x28
    __cpu_disable+0x0/0xbc
    
    CRs-Fixed: 2647192
    Change-Id: I5dc625f0791aff9738b2128f99c79d7e0dadf26d
    Signed-off-by: Subash Abhinov Kasiviswanathan <subashab@codeaurora.org>

--- drivers/net/ethernet/qualcomm/rmnet/rmnet_map.h
+++ drivers/net/ethernet/qualcomm/rmnet/rmnet_map.h
@@ -180,18 +180,10 @@ struct rmnet_map_dl_ind_trl {
 
 struct rmnet_map_dl_ind {
 	u8 priority;
-	union {
-		void (*dl_hdr_handler)(struct rmnet_map_dl_ind_hdr *);
-		void (*dl_hdr_handler_v2)(struct rmnet_map_dl_ind_hdr *,
-					  struct
-					  rmnet_map_control_command_header *);
-	} __aligned(1);
-	union {
-		void (*dl_trl_handler)(struct rmnet_map_dl_ind_trl *);
-		void (*dl_trl_handler_v2)(struct rmnet_map_dl_ind_trl *,
-					  struct
-					  rmnet_map_control_command_header *);
-	} __aligned(1);
+	void (*dl_hdr_handler_v2)(struct rmnet_map_dl_ind_hdr *,
+				  struct rmnet_map_control_command_header *);
+	void (*dl_trl_handler_v2)(struct rmnet_map_dl_ind_trl *,
+				  struct rmnet_map_control_command_header *);
 	struct list_head list;
 };
 
@@ -273,13 +265,9 @@ void rmnet_map_tx_aggregate_init(struct rmnet_port *port);
 void rmnet_map_tx_aggregate_exit(struct rmnet_port *port);
 void rmnet_map_update_ul_agg_config(struct rmnet_port *port, u16 size,
 				    u8 count, u8 features, u32 time);
-void rmnet_map_dl_hdr_notify(struct rmnet_port *port,
-			     struct rmnet_map_dl_ind_hdr *dl_hdr);
 void rmnet_map_dl_hdr_notify_v2(struct rmnet_port *port,
 				struct rmnet_map_dl_ind_hdr *dl_hdr,
 				struct rmnet_map_control_command_header *qcmd);
-void rmnet_map_dl_trl_notify(struct rmnet_port *port,
-			     struct rmnet_map_dl_ind_trl *dltrl);
 void rmnet_map_dl_trl_notify_v2(struct rmnet_port *port,
 				struct rmnet_map_dl_ind_trl *dltrl,
 				struct rmnet_map_control_command_header *qcmd);
